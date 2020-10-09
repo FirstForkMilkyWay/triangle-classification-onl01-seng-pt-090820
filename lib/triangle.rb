0a46f7a1e6b55ddbe8a4f8de76bc1b236592c6f0
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-10-09 15:08:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,9 +9,6 @@ class Triangle
   end 
   
   def kind 
-    #scalene has no equal sides 
-    #isosceles has 2 equal sides 
-    #equilateral has 3 equal sides 
     
     if @side1>0 && @side2>0 && @side3>0 && @side1 + @side2 > @side3 && @side1 != @side2 && @side1 != @side3 && @side2 != @side3
         return :scalene 
