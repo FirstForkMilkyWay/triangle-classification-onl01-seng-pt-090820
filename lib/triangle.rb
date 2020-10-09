1058aad44f12256101f02d4d0354f5fead7907da
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-10-09 14:44:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,6 +17,8 @@ class Triangle
         return :scalene 
     elsif @side1>0 && @side2>0 && @side3>0 && @side1 + @side2 > @side3 && @side1 == @side2 && @side1 != @side3 && @side2 != @side3   
         return :isosceles 
+     elsif @side1>0 && @side2>0 && @side3>0 && @side1 + @side2 > @side3 && @side1 != @side2 && @side1 != @side3 && @side2 == @side3   
+        return :isosceles
     elsif @side1>0 && @side2>0 && @side3>0 && @side1 + @side2 > @side3 && @side1 == @side2 && @side1 == @side3 && @side2 == @side3
       return :equilateral 
     # else
