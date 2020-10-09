56daa75946f02d030226023eb076c59da356bfbd
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-10-09 14:30:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,30 +13,30 @@ class Triangle
     #isosceles has 2 equal sides 
     #equilateral has 3 equal sides 
     
-    if side1>0 && side2>0 && side3>0 && side1 + side2 > side3 && side1 != side2 && side1 != side3 && side2 != side3
+    if @side1>0 && @side2>0 && @side3>0 && @side1 + @side2 > @side3 && @side1 != @side2 && @side1 != @side3 && @side2 != @side3
         return :scalene 
     elsif 
-      side1 == side2 
+      @side1 == @side2 
         return :isosceles 
     elsif 
-      side1 == side2 && side1 == side3 && side2 == side3 
+      @side1 == @side2 && @side1 == @side3 && @side2 == @side3 
       return :equilateral 
-    # else
-    #   side1 + side2 < side3 
-    #     begin
-    #     raise PartnerError
-    #   rescue PartnerError => error
-    #       puts error.message
-    #   end
+     else
+       @side1 + @side2 < @side3 
+         begin
+         raise PartnerError
+       rescue PartnerError => error
+           puts error.message
+       end
     end 
   
   
-#   class TriangleError < StandardError
-#     def message 
-#       puts "TriangleError"
-#     end 
-#   end 
-# end 
+   class TriangleError < StandardError
+     def message 
+      puts "TriangleError"
+     end 
+  end 
+end 
 
 end 
 end
