7e94e072c3a892b1af8a28742f3890f51cb00bfa
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-10-09 14:58:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,7 +23,7 @@ class Triangle
         return :isosceles
     elsif @side1>0 && @side2>0 && @side3>0 && @side1 + @side2 > @side3 && @side1 == @side2 && @side1 == @side3 && @side2 == @side3
       return :equilateral 
-     else
+     elsif
       @side1 <= 0 && @side2 <= 0 && @side3 <= 0 
          begin
          raise PartnerError
