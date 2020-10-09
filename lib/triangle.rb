98b5ebe0d8d12266e8a569a55f7267f8cd57b86f
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-10-09 15:15:22 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,39 +1,30 @@
 class Triangle
-  
-  attr_accessor :equilateral, :isosceles, :scalene
-  
-  def initialize(side1,side2,side3)
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
-  end 
-  
-  def kind 
-    
-    if @side1>0 && @side2>0 && @side3>0 && @side1 + @side2 > @side3 && @side1 != @side2 && @side1 != @side3 && @side2 != @side3
-        return :scalene 
-    elsif @side1>0 && @side2>0 && @side3>0 && @side1 + @side2 > @side3 && @side1 == @side2 && @side1 != @side3 && @side2 != @side3   
-        return :isosceles 
-     elsif @side1>0 && @side2>0 && @side3>0 && @side1 + @side2 > @side3 && @side1 != @side2 && @side1 != @side3 && @side2 == @side3   
-        return :isosceles
-     elsif @side1>0 && @side2>0 && @side3>0 && @side1 + @side2 > @side3 && @side1 != @side2 && @side1 == @side3 && @side2 != @side3   
-        return :isosceles
-    elsif @side1>0 && @side2>0 && @side3>0 && @side1 + @side2 > @side3 && @side1 == @side2 && @side1 == @side3 && @side2 == @side3
-      return :equilateral 
-     elsif
-      @side1 <= 0 && @side2 <= 0 && @side3 <= 0 
-         begin
-         raise TriangleError
-       rescue TriangleError => error
-           puts error.message
-       end
-    end 
+  def initialize(side_1,side_2,side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
   end
-  
-  class TriangleError < StandardError
-  
-  end 
+
+  def kind()
+    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+      raise TriangleError
+    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+      raise TriangleError
+    else
+      if (@side_1 == @side_2) && (@side_2 == @side_3)
+        :equilateral
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        :scalene
+      end
+    end
+
+  end
 
 end
 
+class TriangleError < StandardError
+  # triangle error code
+end
 
