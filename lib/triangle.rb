2f5e1b9e9857b7c6f4f9cb39a505e1bb1e6e11f4
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-10-09 15:19:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,21 +1,21 @@
 class Triangle
-  def initialize(side_1,side_2,side_3)
-    @side_1 = side_1
-    @side_2 = side_2
-    @side_3 = side_3
+  def initialize(side1,side2,side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
   end
 
   def kind()
-    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
-      raise RangeError
-    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
-      raise RangeError
+    if (@side1 <= 0) || (@side2 <= 0) || (@side3 <= 0)
+      raise TriangleError
+    elsif (@side1+@side2 <= @side3) || (@side1+@side3 <= @side2) || (@side2+@side3 <= @side1)
+      raise TriangleError
     else
-      if (@side_1 == @side_2) && (@side_2 == @side_3)
+      if (@side1 == @side2) && (@side2 == @side3)
         :equilateral
-      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+      elsif (@side1 == @side2) || (@side2 == @side3) || (@side1 == @side3)
         :isosceles
-      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+      elsif (@side1 != @side2) && (@side_2 != @side3) && (@side1 != @side3)
         :scalene
       end
     end
@@ -24,7 +24,7 @@ class Triangle
 
 end
 
-class RangeError < StandardError
+class TriangleError < StandardError
   
 end
 
